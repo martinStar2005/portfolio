b2c7b63027b5b94ef8d198606cdfa331a8be6105
Martin Larsen <05larmar@taby.se>
2025-01-14 15:51:39 +0100

    implement navbar

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,5 +1,20 @@
+import img from "../assets/navbarImg.png"
+import styles from "./Layout.module.css"
+
+
 export default function Layout() {
     return(
-        <p>layyyyy</p>
+        <>
+            <nav className={styles.container}>
+                <img src={img} alt="navigation image" />
+                <div>
+                    <p>Home</p>
+                    <p>About Me</p>
+                    <p>Portfolio</p>
+                    <p>Blog</p>
+                    <p>Contact</p>
+                </div>
+            </nav>
+        </>
     )
 }
